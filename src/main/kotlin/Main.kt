dd7deb46e8d054ddedd7a709686490e4c622daeb
Christopher Osthues <christopher.osthues@gmail.com>
2023-10-17 23:00:57 +0200

    Example for tabs

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,13 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Row
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
@@ -15,15 +15,31 @@ import androidx.compose.ui.window.application
 @Preview
 fun app() {
     var text by remember { mutableStateOf("Hello, World!") }
+    var tabIndex by remember { mutableStateOf(0) }
+
+    val tabs = listOf("1", "2")
 
     MaterialTheme {
-        Row {
-            Button(onClick = {
-                text = "Hello, Desktop!"
-            }) {
-                Text(text)
+        Column(modifier = Modifier.fillMaxWidth()) {
+            TabRow(selectedTabIndex = tabIndex) {
+                tabs.forEachIndexed { index, title ->
+                    Tab(
+                        text = { Text(title) },
+                        selected = tabIndex == index,
+                        onClick = { tabIndex = index },
+                    )
+                }
             }
         }
+        when (tabIndex) {
+            0 -> Text("Hello from 1")
+            1 -> Text("Adios")
+        }
+//            Button(onClick = {
+//                text = "Hello, Desktop!"
+//            }) {
+//                Text(text)
+//            }
     }
 }
 
