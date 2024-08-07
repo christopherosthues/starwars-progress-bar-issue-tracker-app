f87cd9fac95ab89876402fae2743c31a52d633f4
Christopher Osthues <christopher.osthues@gmail.com>
2024-08-08 00:10:39 +0200

    feat: Add OS name and version to Desktop app

--- composeApp/src/desktopMain/kotlin/Platform.jvm.kt
+++ composeApp/src/desktopMain/kotlin/Platform.jvm.kt
@@ -1,5 +1,5 @@
 class JVMPlatform: Platform {
-    override val name: String = "Java ${System.getProperty("java.version")}"
+    override val name: String = "${System.getProperty("os.name")} (${System.getProperty("os.version")}) running Java ${System.getProperty("java.version")}"
 }
 
 actual fun getPlatform(): Platform = JVMPlatform()
